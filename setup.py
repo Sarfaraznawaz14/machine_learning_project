4efdaa33e597d775e79820fb4ad227d61f5f6e35
Sarfaraznawaz14 <nawazme.rymec@gmail.com>
2023-03-05 17:06:20 +0400

    requirements.txt file updated with -e .

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 
 
 # declaring variables for setup functions
 PROJECT_NAME = "housing-prediction"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 AUTHOR = "sarfaraz"
 DESCRIPTION = "THIS IS MY 1ST PROJECT"
-PACKAGES = ["housing"]
 REQUIREMENT_FILE_NAME = "requirements.txt"
 
 def get_requirements_list()->list[str]:
@@ -20,13 +19,13 @@ def get_requirements_list()->list[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 setup(
 name=PROJECT_NAME ,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
-packages=PACKAGES,
+packages=find_packages(),
 install_requires = get_requirements_list()
 
 )
